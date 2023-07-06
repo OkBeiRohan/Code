86263cd42210e81679b35e15e1a157bc09d0163e
Rohan K <rohithkamal@gmail.com>
2023-07-06 13:41:18 +0530

    simple sum

---
+++ Basic C/Sum/Simple/sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Sum is: " << a + b << "\n";
+    return 0;
+}
