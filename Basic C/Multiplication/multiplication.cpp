ee4da101d388e871585ceeb728b9ed24793c8e64
Rohan K <rohithkamal@gmail.com>
2023-07-06 12:31:52 +0530

    multiplication and sum dynamic

---
+++ Basic C/Multiplication/multiplication.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+double multiply(int count, double *numbers)
+{
+    double result = 1;
+    for (int i = 0; i < count; i++)
+    {
+        result *= numbers[i];
+    }
+    return result;
+}
+
+int main()
+{
+    int count;
+    cout << "Enter count of numbers to multiply: ";
+    cin >> count;
+    double *numbers = new double[count];
+    cout << "Enter " << count << " numbers to multiply: ";
+    for (int i = 0; i < count; i++)
+    {
+        cin >> numbers[i];
+    }
+    double result = multiply(count, numbers);
+    cout << "Multiplication result is: " << result << "\n";
+    delete[] numbers;
+    return 0;
+}
--- Basic C/Sum/sum.cpp
+++ Basic C/Sum/sum.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 using namespace std;
 
+double sum(int count, double *numbers)
+{
+    double result = 0;
+    for (int i = 0; i < count; i++)
+    {
+        result += numbers[i];
+    }
+    return result;
+}
+
 int main()
 {
-    double a, b, sum;
-    cout << "Enter two numbers to add: ";
-    cin >> a >> b;
-    sum = a + b;
-    cout << "Sum is: " << sum << "\n";
+    int count;
+    cout << "Enter count of numbers to sum: ";
+    cin >> count;
+    double *numbers = new double[count];
+    cout << "Enter " << count << " numbers to sum: ";
+    for (int i = 0; i < count; i++)
+    {
+        cin >> numbers[i];
+    }
+    double result = sum(count, numbers);
+    cout << "Sum result is: " << result << "\n";
+    delete[] numbers;
     return 0;
 }
