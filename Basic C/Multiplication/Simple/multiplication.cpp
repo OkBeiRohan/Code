9d4d9b381904095951a40db0576feb4b5d73541a
Rohan K <rohithkamal@gmail.com>
2023-07-06 12:34:31 +0530

    dyn and simp

---
+++ Basic C/Multiplication/Simple/multiplication.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    cout << "Enter four numbers: ";
+    cin >> a >> b >> c >> d;
+    cout << "Multiplication result is: " << a * b * c * d << "\n";
+    return 0;
+}
