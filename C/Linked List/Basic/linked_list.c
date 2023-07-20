be6adc76fabebce9f39fadbe8e1a054018ef6607
Rohan K <rohithkamal@gmail.com>
2023-07-20 15:07:56 +0530

    double linked list

--- C/Linked List/Basic/linked_list.c
+++ C/Linked List/Basic/linked_list.c
@@ -9,21 +9,35 @@ struct node_s
 typedef struct node_s node;
 
 void create_node(int);
+void delete_node(int);
 void display_list();
+int find_node(int);
 
 int main()
 {
     int n, data;
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
+    printf("Enter the data for %d nodes: ", n);
     for (int i = 0; i < n; i++)
     {
-        printf("Enter the data for node %d: ", i + 1);
         scanf("%d", &data);
         create_node(data);
     }
 
+    printf("\nThe linked list is: ");
     display_list();
+
+    printf("\n\nEnter the position of the node to search: ");
+    scanf("%d", &n);
+    printf("Data in position %d is: %d", n, find_node(n));
+
+    printf("\n\nEnter the position of the node to delete: ");
+    scanf("%d", &n);
+    delete_node(n);
+    printf("The linked list after deletion is: ");
+    display_list();
+
     return 0;
 }
 
@@ -56,4 +70,24 @@ void display_list()
         if (temp != NULL)
             printf(" -> ");
     }
+}
+
+int find_node(int n)
+{
+    node *temp = head;
+    for (int i = 1; i < n; i++)
+    {
+        temp = temp->next;
+    }
+    return temp->data;
+}
+
+void delete_node(int n)
+{
+    node *temp = head;
+    for (int i = 1; i < n - 1; i++)
+    {
+        temp = temp->next;
+    }
+    temp->next = temp->next->next;
 }
---
+++ C/Linked List/Double Pointed/double_linked_list.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node_s
+{
+    int data;
+    struct node_s *next;
+    struct node_s *prev;
+} *head = NULL, *tail = NULL;
+typedef struct node_s node;
+
+void create_node(int);
+void delete_node(int);
+void display_list();
+int find_node(int);
+
+int main()
+{
+    int n, data;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+    printf("Enter the data for %d nodes: ", n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &data);
+        create_node(data);
+    }
+
+    printf("\nThe linked list is: ");
+    display_list();
+
+    printf("\n\nEnter the position of the node to search: ");
+    scanf("%d", &n);
+    printf("Data in position %d is: %d", n, find_node(n));
+
+    printf("\n\nEnter the position of the node to delete: ");
+    scanf("%d", &n);
+    delete_node(n);
+    printf("The linked list after deletion is: ");
+    display_list();
+
+    return 0;
+}
+
+void create_node(int data)
+{
+    node *new_node = (node *)malloc(sizeof(node));
+    new_node->data = data;
+    new_node->next = NULL;
+    new_node->prev = NULL;
+
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+    }
+    else
+    {
+        tail->next = new_node;
+        new_node->prev = tail;
+        tail = new_node;
+    }
+}
+
+void delete_node(int pos)
+{
+    node *temp = head;
+    if (pos == 1)
+    {
+        head = head->next;
+        head->prev = NULL;
+        free(temp);
+    }
+    else
+    {
+        for (int i = 1; i < pos - 1; i++)
+            temp = temp->next;
+        node *temp2 = temp->next;
+        temp->next = temp2->next;
+        temp2->next->prev = temp;
+        free(temp2);
+    }
+}
+
+void display_list()
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int find_node(int pos)
+{
+    node *temp = head;
+    for (int i = 1; i < pos; i++)
+        temp = temp->next;
+    return temp->data;
+}
